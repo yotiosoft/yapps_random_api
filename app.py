c7b830d52d52238a6aa9213c9660306b00bccf66
YotioSoft <ytani0323@gmail.com>
2022-06-10 03:32:34 +0900

    update

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 from flask import Flask
-import numpy
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World!!"
+    return "Hello World!"
 
 if __name__ == '__main__':
     app.run()
