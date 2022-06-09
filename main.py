1a6e022a5892b5352d985978bde6ad15cd883440
YotioSoft <ytani0323@gmail.com>
2022-06-10 02:48:10 +0900

    first test commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from flask import Flask
+import numpy
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello World!"
+
+if __name__ == '__main__':
+    app.run()
