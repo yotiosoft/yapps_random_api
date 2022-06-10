c1cce6c83a866dffeb95db180f1f665163d75a1b
YotioSoft <ytani0323@gmail.com>
2022-06-11 02:38:53 +0900

    change return value type to json

--- app.py
+++ app.py
@@ -2,8 +2,10 @@
 import sys
 from flask import Flask, request
 import random
+import json
 
 app = Flask(__name__)
+jrand = {}
 
 
 class query_class:
@@ -16,16 +18,19 @@ class query_class:
         self.lambd = 0.0
         self.alpha = 0.0
         self.beta = 0.0
+        self.trials = 1
         self.type = ""
+        self.err = {}
 
     def get(self, distribution):
         try:
-            self.type = request.args.get("type")
-            if self.type is None:
-                self.type = "int"
-            else:
-                if self.type != "float" and self.type != "int":
-                    self.type = "int"
+            arg_type = request.args.get("type")
+            if self.type is not None:
+                self.type = arg_type
+
+            arg_trials = request.args.get("trials")
+            if arg_trials is not None:
+                self.trials = int(arg_trials)
                     
             if distribution == "uniform" or distribution == "triangular":
                 req_min = request.args.get("min")
@@ -35,12 +40,12 @@ class query_class:
                     if req_min is None:
                         self.min = -sys.maxsize
                     else:
-                        self.min = int(req_min)
+                        self.min = float(req_min)
                     
                     if req_max is None:
                         self.max = sys.maxsize
                     else:
-                        self.max = int(req_max)
+                        self.max = float(req_max)
                 else:
                     if req_min is None:
                         self.min = sys.float_info.min
@@ -68,12 +73,12 @@ class query_class:
                 if req_mu is None:
                     self.mu = 0
                 else:
-                    self.mu = int(req_mu)
+                    self.mu = float(req_mu)
                 
                 if req_sigma is None:
                     self.sigma = 1
                 else:
-                    self.sigma = int(req_sigma)
+                    self.sigma = float(req_sigma)
                 return
 
             elif distribution == "lambda":
@@ -92,105 +97,167 @@ class query_class:
                 if req_alpha is None:
                     self.alpha = 1.0
                 else:
-                    self.alpha = int(req_alpha)
+                    self.alpha = float(req_alpha)
                 
                 if req_beta is None:
                     self.beta = 2.0
                 else:
-                    self.beta = int(req_beta)
+                    self.beta = float(req_beta)
                 return
+
         except ValueError:
-            return "Error: 引数が正しくありません"
+            self.err["error_num"] = 1
+            self.err["error_message"] = "Error: Arguments are incorrect"
+            return
 
 
 @app.route('/')
 def root_index():
-    return "Successfully accessed."
+    return "Hello World!"
 
-@app.route('/uniform', methods=["GET"])
+@app.route('/random/uniform', methods=["GET"])
 def uniform_index():
+    rand = []
+    jrand = {}
     query = query_class()
     query.get("uniform")
 
+    if 'error_num' in query.err:
+        return json.dumps(query.err)
+
     try:
-        if query.type == "int":
-            rand = str(random.randint(query.min, query.max))
-        else:
-            rand = random.uniform(query.min, query.max)
+        for i in range(query.trials):
+            if query.type == "int":
+                rand.append(random.randint(query.min, query.max))
+            else:
+                rand.append(random.uniform(query.min, query.max))
     except ValueError:
-        return "Error: 正しいパラメータが指定されていません"
+        jrand["error_num"] = 2
+        jrand["error_message"] = "Error: Wrong parameter"
+        return json.dumps(jrand)
 
-    return str(rand)
+    jrand["rand_array"] = rand
+    return json.dumps(jrand)
 
-@app.route('/normal', methods=["GET"])
+@app.route('/random/normal', methods=["GET"])
 def normal_index():
+    rand = []
+    jrand = {}
     query = query_class()
     query.get("normal")
 
+    if 'error_num' in query.err:
+        return json.dumps(query.err)
+
     try:
-        rand = random.gauss(query.mu, query.sigma)
-        if query.type == "int":
-            rand = int(rand)
+        for i in range(query.trials):
+            rand_temp = random.gauss(query.mu, query.sigma)
+            if query.type == "int":
+                rand_temp = int(rand_temp)
+            rand.append(rand_temp)
     except ValueError:
-        return "Error: 正しいパラメータが指定されていません"
+        jrand["error_num"] = 2
+        jrand["error_message"] = "Error: Wrong parameter"
+        return json.dumps(jrand)
 
-    return str(rand)
+    jrand["rand_array"] = rand
+    return json.dumps(jrand)
 
-@app.route('/beta', methods=["GET"])
+@app.route('/random/beta', methods=["GET"])
 def beta_index():
+    rand = []
+    jrand = {}
     query = query_class()
     query.get("beta")
 
+    if 'error_num' in query.err:
+        return json.dumps(query.err)
+
     try:
-        rand = random.betavariate(query.alpha, query.beta)
-        if query.type == "int":
-            rand = int(rand)
+        for i in range(query.trials):
+            rand_temp = random.betavariate(query.alpha, query.beta)
+            if query.type == "int":
+                rand_temp = int(rand_temp)
+            rand.append(rand_temp)
     except ValueError:
-        return "Error: 正しいパラメータが指定されていません"
+        jrand["error_num"] = 2
+        jrand["error_message"] = "Error: Wrong parameter"
+        return json.dumps(jrand)
 
-    return str(rand)
+    jrand["rand_array"] = rand
+    return json.dumps(jrand)
 
-@app.route('/triangular', methods=["GET"])
+@app.route('/random/triangular', methods=["GET"])
 def triangular_index():
+    rand = []
+    jrand = {}
     query = query_class()
     query.get("triangular")
 
+    if 'error_num' in query.err:
+        return json.dumps(query.err)
+
     try:
-        rand = random.triangular(query.min, query.max, query.mode)
-        if query.type == "int":
-            rand = int(rand)
+        for i in range(query.trials):
+            rand_temp = random.triangular(query.min, query.max, query.mode)
+            if query.type == "int":
+                rand = int(rand_temp)
+            rand.append(rand_temp)
     except ValueError:
-        return "Error: 正しいパラメータが指定されていません"
+        jrand["error_num"] = 2
+        jrand["error_message"] = "Error: Wrong parameter"
+        return json.dumps(jrand)
 
-    return str(rand)
+    jrand["rand_array"] = rand
+    return json.dumps(jrand)
 
-@app.route('/lambda', methods=["GET"])
+@app.route('/random/lambda', methods=["GET"])
 def lambda_index():
+    rand = []
+    jrand = {}
     query = query_class()
     query.get("lambda")
 
+    if 'error_num' in query.err:
+        return json.dumps(query.err)
+
     try:
-        rand = random.expovariate(query.lambd)
-        if query.type == "int":
-            rand = int(rand)
+        for i in range(query.trials):
+            rand_temp = random.expovariate(query.lambd)
+            if query.type == "int":
+                rand = int(rand_temp)
+            rand.append(rand_temp)
     except ValueError:
-        return "Error: 正しいパラメータが指定されていません"
+        jrand["error_num"] = 2
+        jrand["error_message"] = "Error: Wrong parameter"
+        return json.dumps(jrand)
 
-    return str(rand)
+    jrand["rand_array"] = rand
+    return json.dumps(jrand)
 
-@app.route('/gamma', methods=["GET"])
+@app.route('/random/gamma', methods=["GET"])
 def gamma_index():
+    rand = []
+    jrand = {}
     query = query_class()
     query.get("gamma")
 
-    try:
-        rand = random.gammavariate(query.alpha, query.beta)
-        if query.type == "int":
-            rand = int(rand)
-    except ValueError:
-        return "Error: 正しいパラメータが指定されていません"
+    if 'error_num' in query.err:
+        return json.dumps(query.err)
 
-    return str(rand)
+    try:
+        for i in range(query.trials):
+            rand_temp = random.gammavariate(query.alpha, query.beta)
+            if query.type == "int":
+                rand = int(rand_temp)
+            rand.append(rand_temp)
+    except ValueError:
+        jrand["error_num"] = 2
+        jrand["error_message"] = "Error: Wrong parameter"
+        return json.dumps(jrand)
+
+    jrand["rand_array"] = rand
+    return json.dumps(jrand)
 
 if __name__ == '__main__':
     app.run()
